a705af9430cb8d4ae58b14de04038efffd6907ec
Daniel Martinez-Gamboa <dmartinezgamboa@gmail.com>
2021-10-09 16:41:31 -0700

    Update to script.js to grab document objects

---
+++ app/static/script.js
@@ -0,0 +1,7 @@
+const timer = document.querySelectorAll(".timer");
+const prompt = document.querySelectorAll(".prompt");
+const inputBox = document.querySelectorAll(".input-box");
+
+inputBox.value = "hello there";
+
+timer.value = "10000";
