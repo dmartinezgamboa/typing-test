248f45058cdec6751c0c1c3383b1242cd89d8a74
Daniel Martinez-Gamboa <dmartinezgamboa@gmail.com>
2021-10-09 18:26:19 -0700

    Implement callback functions for event listeners
    Declare objects with html elements.
    Event listener to start button added.
    Event listener added to inputBox.

--- app/static/script.js
+++ app/static/script.js
@@ -1,7 +1,56 @@
-const timer = document.querySelectorAll(".timer");
-const prompt = document.querySelectorAll(".prompt");
-const inputBox = document.querySelectorAll(".input-box");
+// TODO: "before input" behaves unexpectedly, one input behind in what is currently in inputBox;
 
-inputBox.value = "hello there";
+const timer = document.querySelector(".timer");
+const prompt = document.querySelector(".prompt");
+const inputBox = document.querySelector("input");
+const startButton = document.querySelector(".start-button");
 
-timer.value = "10000";
+inputBox.addEventListener("beforeinput", onUserKey);
+startButton.addEventListener("click", start);
+
+var currentInput = ""; // user state variables
+var currentIndex = 0;
+
+testSentenceArray = [
+  "The ",
+  "dog ",
+  "jumped ",
+  "the ",
+  "over ",
+  "the ",
+  "moon.",
+];
+
+function start() {
+  //test function
+  inputBox.value = "";
+  currentInput = "";
+  currentIndex = 0;
+  console.log("script has started");
+}
+
+function log() {
+  console.log("length of testSentenceArray is ", testSentenceArray.length);
+  console.log("currentIndex is ", currentIndex);
+  console.log("currentInput is ", currentInput);
+  console.log();
+}
+
+function victory() {
+  // test function
+  console.log("victory!");
+}
+
+function onUserKey() {
+  log();
+  currentInput = inputBox.value;
+  if (inputBox.value == testSentenceArray[currentIndex]) {
+    currentIndex++;
+    currentInput = "";
+  }
+  if (currentIndex == testSentenceArray.length - 1) {
+    console.log("congratulations");
+    return;
+  }
+  return;
+}
