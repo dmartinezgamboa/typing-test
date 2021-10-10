08307cbc369bfbda4981d5371464f15942e145ce
Daniel Martinez-Gamboa <dmartinezgamboa@gmail.com>
2021-10-09 19:55:19 -0700

    Fix bug related to onUserKey function
    onUserKey is behind input due to incorrect arg passed to inputBox.addEventListener function
    Changing argument from 'beforeinput' to 'input' removes issue.
    Includes misc. changes to onUserKey if statement conditionals and when log function is called.

--- app/static/script.js
+++ app/static/script.js
@@ -1,56 +1,51 @@
-// TODO: "before input" behaves unexpectedly, one input behind in what is currently in inputBox;
-
 const timer = document.querySelector(".timer");
 const prompt = document.querySelector(".prompt");
 const inputBox = document.querySelector("input");
 const startButton = document.querySelector(".start-button");
 
-inputBox.addEventListener("beforeinput", onUserKey);
-startButton.addEventListener("click", start);
+inputBox.addEventListener("input", onUserKey);
+startButton.addEventListener("click", reset); // refresh state to default
 
 var currentInput = ""; // user state variables
 var currentIndex = 0;
 
-testSentenceArray = [
-  "The ",
-  "dog ",
-  "jumped ",
-  "the ",
-  "over ",
-  "the ",
-  "moon.",
-];
-
-function start() {
-  //test function
-  inputBox.value = "";
-  currentInput = "";
-  currentIndex = 0;
-  console.log("script has started");
-}
-
-function log() {
-  console.log("length of testSentenceArray is ", testSentenceArray.length);
-  console.log("currentIndex is ", currentIndex);
-  console.log("currentInput is ", currentInput);
-  console.log();
-}
-
-function victory() {
-  // test function
-  console.log("victory!");
-}
+testSentenceArray = ["The ", "dog ", "jumped ", "over ", "the ", "moon."];
 
 function onUserKey() {
-  log();
   currentInput = inputBox.value;
+
   if (inputBox.value == testSentenceArray[currentIndex]) {
     currentIndex++;
     currentInput = "";
+    inputBox.value = "";
+    if (currentIndex == testSentenceArray.length) {
+      log();
+      victory();
+      return;
+    }
   }
-  if (currentIndex == testSentenceArray.length - 1) {
-    console.log("congratulations");
-    return;
-  }
+  log();
   return;
+}
+
+function log() {
+  console.log(
+    "testSentenceArray has ",
+    testSentenceArray.length - 1,
+    " indexes"
+  );
+  console.log("currentIndex is ", currentIndex);
+  console.log("currentInput is ", currentInput);
+  console.log("current word is ", testSentenceArray[currentIndex]);
+}
+
+function reset() {
+  inputBox.value = "";
+  currentInput = "";
+  currentIndex = 0;
+  console.log("state reset");
+}
+
+function victory() {
+  console.log("victory!");
 }
